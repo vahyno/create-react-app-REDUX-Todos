8a9a2e9fefac7c1cbb841338479e5a15efd96cf0
vahyno <vahyno@gmail.com>
2019-02-23 18:52:56 -0800

    The store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers';
+import middleware from './middleware';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const store = createStore(reducer, middleware);
+
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 
